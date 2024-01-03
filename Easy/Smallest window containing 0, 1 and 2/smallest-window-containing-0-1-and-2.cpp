85ee9a387ae011bdac1cb81f25578a1f01f81d6b
Raj Mansuriya <84504923+rajmansuriya21@users.noreply.github.com>
2024-01-03 18:31:23 +0530

    Updated Solution - GfG to GitHub

--- Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.cpp
+++ Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.cpp
@@ -11,28 +11,22 @@ class Solution {
    int smallestSubstring(string S) {
     int n = S.length();
     
-    int count[3] = {0}; // To store the count of 0, 1, and 2 in the current window
-    int start = 0; // Start index of the window
-    int minLength = INT_MAX; // Initialize the minimum length to a large value
+    int count[3] = {0};
+    int start = 0;
+    int minLength = INT_MAX;
     
     for (int end = 0; end < n; ++end) {
-        // Update count for the current character in the window
         count[S[end] - '0']++;
         
-        // Try to minimize the window by moving the start index
         while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-            // Update minLength
             minLength = min(minLength, end - start + 1);
-            
-            // Move the start index and update count accordingly
             count[S[start] - '0']--;
             start++;
         }
     }
     
-    // If minLength is still INT_MAX, no valid substring found
     return (minLength == INT_MAX) ? -1 : minLength;
- }
+  }
 };
 
 
