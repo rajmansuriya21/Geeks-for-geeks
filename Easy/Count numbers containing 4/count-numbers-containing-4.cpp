dc333e11a4fa6f816b172ca4dd91196dca4e2be7
Raj Mansuriya <84504923+rajmansuriya21@users.noreply.github.com>
2024-06-12 11:36:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count numbers containing 4/count-numbers-containing-4.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        // code here
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            int num=i;
+            while(num>0){
+            int rem=num%10;
+            //int div=i/10;
+            if(rem==4){
+                ans++;
+                break;
+            }
+            num=num/10;
+        }
+        }
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        int res = obj.countNumberswith4(n);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
